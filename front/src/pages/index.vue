944a31383362c608156192dade498caf68d98002
Louise P <pieri.louisse@gmail.com>
2020-02-18 13:22:36 +0100

    [Front]: WIP

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,14 +1,13 @@
 <template>
-<div>
-  <SectionParticales></SectionParticales>
-  <TemplateSection :data="xpData"></TemplateSection>
-  <TemplateSection :data="pcData"></TemplateSection>
-</div>
+  <div>
+    <SectionParticales></SectionParticales>
+    <!--<template-section :data="xpData"></template-section>-->
+  </div>
 </template>
 
 <script>
-import SectionParticales from '../components/index/sectionParticales'
-import TemplateSection from '../components/index/templateSection'
+import SectionParticales from '@/components/index/sectionParticles'
+import TemplateSection from '@/components/index/templateSection'
 
 export default {
 	name: 'Index',
@@ -19,16 +18,10 @@ export default {
   data () {
 	  return {
 	    xpData: {
-	      title: "Bienvenue sur louise.lgbt",
-        profilePicture: "static/img/logo.png",
-        textH3: "Mon nom est Louise Pieri, je suis une jeune développeuse passionné par la blockchain, la sécurité et les réseaux décentraliser...",
-        textH4: null
-      },
-      pcData: {
-	      title: "Mon pacours..",
+	      title: null,
         profilePicture: null,
-        textH4: "J'ai commencé à coder en 2017 quand je suis rentrée à 42 Paris",
-        textH3: null
+        textH3: null,
+        textH4: null
       }
     }
   }
